33d89dd19221c1909b7d0d96bb5b9b01d1d73ba0
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-09-25 18:25:30 +0500

    Task 4

---
+++ Tyuiu.AbdullinAI.Sprint1.Task4.V7.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint1.Task4.V7.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint1.Task4.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 4;
+            y = 4;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(res, 0.078);
+
+
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint1.Task4.V7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint1.Task4.V7.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint1.Task4.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране. Ответ округлите*");
+            Console.WriteLine("* до 3 знаков после запятой.                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y; 
+            Console.WriteLine("Введите значение x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+                     
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ваш результат по формуле (1 + sqrt(x*y)) / (x - 3*y)^2: " + ds.Calculate(x, y));
+            Console.ReadKey();
+
+        }
+    }
+}
