45b61e69f8da0d7efa4686bb142f9d1f391ebc8d
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-09-27 15:51:58 +0500

    Task5

---
+++ Tyuiu.AbdullinAI.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AbdullinAI.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 12;
+            var res = ds.FahrenheitToСelsius(x);
+            Assert.AreEqual(res, -11);
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано значение температуры в градусах Фаренгейта. Определить значение    *");
+            Console.WriteLine("* этой же температуры в градусах Цельсия. Ответ привести к целому         *");
+            Console.WriteLine("* с помощью класса Convert.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            Console.WriteLine("Введите значение градусов в Фаренгейтах: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение в градусах Цельсия: " + ds.FahrenheitToСelsius(x));
+            Console.ReadKey();
+        }
+    }
+}
